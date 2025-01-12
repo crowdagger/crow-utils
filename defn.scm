0c9c922a37c1d668a7f563112d3019a1a5849505
Lizzie Crowdagger <lizzie@crowdagger.fr>
2025-01-12 01:37:09 +0100

    Initial commit

---
+++ defn.scm
@@ -0,0 +1,33 @@
+(define-module (crow-utils defn)
+  #:export (defn))
+
+;;;; Variant of define, allowing to specify in and output "types" as predicates,
+;;;; with a syntax that looks a bit like static typing but is more like a contract:
+;;;; I guess?
+
+(define (assert pred? val)
+  "Throws an exception if val does not satisfy pred"
+  (when (not (pred? val))
+    (raise-exception (format #f "Value '~a' does not satisfy ~a"
+                             val
+                             pred?))))
+
+;; Ok this is probably not a good idea?
+(define -> '())
+
+(define-syntax defn
+  (syntax-rules ()
+    ((_ (name args ...) (test ...) body body* ...)
+     (define (name args ...)
+       (let ([all-args (list args ...)])
+         (map assert
+              (list-head (list test ...) (length all-args))
+              all-args))
+       (let ((return 
+              (begin 
+                body
+                body*
+                ...)))
+         (assert (car (last-pair (list test ...))) return)
+         return)))))
+
