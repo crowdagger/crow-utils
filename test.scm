f1acc657b3af626e0de415baa1525255efd924c3
Lizzie Crowdagger <lizzie@crowdagger.fr>
2025-01-20 00:22:19 +0100

    Add tests

--- test.scm
+++ test.scm
@@ -1,5 +1,26 @@
 (add-to-load-path "..")
-(import (crow-utils checked))
+(install-r7rs!)
+
+(import (scheme base)
+        (srfi srfi-64)
+        (crow-utils checked))
+
+(test-begin "Checked")
+(test-group "defn"
+  (defn (double x)
+    (number? -> number?)
+    #:doc "Doubles a number"
+    (* 2 x))
+  (double 42)
+  (test-error (double "42"))
+  (defn (divide x y)
+    (integer? integer? -> integer?)
+    (/ x y))
+  (test-assert (divide 4 2))
+  (test-error (divide 4.2 2.1))
+  (test-error (divide 4 3))
+  )
+(test-end "Checked")
 
 (defn (double x)
   (number? -> number?)
@@ -11,3 +32,6 @@
 
 (define-checked (add [x number?] [y number?])
   (+ x y))
+
+(add 2 3)
+
