f93d0ace98206886211ec8d9056b820ef789de00
Lizzie Crowdagger <lizzie@crowdagger.fr>
2025-01-29 01:47:59 +0100

    Take optionals delimiters as arguments

--- template.scm
+++ template.scm
@@ -1,33 +1,59 @@
 (define-library (crow-utils template)
   (import (scheme base)
+          (scheme write)
           (srfi srfi-13)
           (srfi srfi-69))
-  (export process-template apply-template)
+  (export process-template apply-template
+          *opening-delimiter* *closing-delimiter*)
   (begin
-    (define (find-closing s)
-      "Search a string for }} and returns ???"
-      (let ([b (string-contains s "}}")])
+    (define (find-closing s opening-delimiter closing-delimiter)
+      "Search a string for the closing delimiter and returns ???"
+      (let ([b (string-contains s closing-delimiter)])
         (if (not b)
-            (error "Unmatched opening brackets" s)
-            (cons (string->symbol
-                   (string-trim-both (substring s 0 b)
-                                     #\ ))
-                  (find-opening (substring s (+ 2 b)))))))
+            (error "Unmatched template's opening delimiter" s)
+            (let ([tag (string-trim-both (substring s 0 b)
+                                         #\ )])
+              (cons (string->symbol tag)
+                    (find-opening
+                     (substring
+                      s
+                      (+ (string-length closing-delimiter)
+                         b))
+                     opening-delimiter
+                     closing-delimiter))))))
     
-    (define (find-opening s)
-      "Search a strïng for {{ and returns ???"
-      (let ([b (string-contains s "{{")])
+    (define (find-opening s opening-delimiter closing-delimiter)
+      "Search a strïng for opening delimiter and returns ???"
+      (let ([b (string-contains s opening-delimiter)])
         (if (not b)
             (list s)
             (cons (substring s 0 b)
                   (find-closing
-                   (substring s (+ 2 b)))))))
+                   (substring s (+ (string-length opening-delimiter)
+                                   b))
+                   opening-delimiter
+                   closing-delimiter)))))
     
-    (define (process-template string)
-      "Transform a template to a list of substrings and symbols"
+    (define (process-template string . rest)
+      "Transform a template to a list of substrings and symbols.
+
+Additional parameters may include strings for opening and closing delimiters"
       (unless (string? string)
-        (error "Argument must be a string" string))
-      (find-opening string))
+        (error "Template must be a string" string))
+      (let ([opening-delimiter
+             (if (> (length rest) 0)
+                 (if (string? (car rest))
+                     (car rest)
+                     (error "Opening delimiter must be a string" (car rest)))
+                 "{{")]
+            [closing-delimiter
+             (if (> (length rest) 1)
+                 (if (string? (cadr rest))
+                     (cadr rest)
+                     (error "Closing delimiter must be a string"
+                            (cadr rest)))
+                 "}}")])
+        (find-opening string opening-delimiter closing-delimiter)))
 
     (define (apply-template template context)
       "Apply template with values given by context
